5def3a861497d36e7fabdd2a98e4a36b5399be61
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-25 19:57:51 -0400

    chore: refactor types (#38)

--- dao_treasury/types.py
+++ dao_treasury/types.py
@@ -1,6 +1,17 @@
-from typing import Literal, NewType
+from typing import TYPE_CHECKING, Awaitable, Callable, Literal, NewType, Union
+
+if TYPE_CHECKING:
+    from dao_treasury.db import TreasuryTx
 
 
 TopLevelCategory = Literal["Revenue", "Cost of Revenue", "Expenses", "Other Income", "Other Expenses", "Ignore"]
 
 TxGroupDbid = NewType("TxGroupDbid", int)
+
+
+TxGroupName = str
+
+SortFunction = Union[
+    Callable[["TreasuryTx"], bool], 
+    Callable[["TreasuryTx"], Awaitable[bool]],
+]
