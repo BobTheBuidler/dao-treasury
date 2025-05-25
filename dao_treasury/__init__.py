0de8699d7333c40a2943b87fbdd77d00a4eb1a90
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-24 20:48:12 -0400

    feat: sort rule subclasses (#14)

--- dao_treasury/__init__.py
+++ dao_treasury/__init__.py
@@ -1,5 +1,22 @@
 from dao_treasury._wallet import TreasuryWallet
+from dao_treasury.sorting import (
+    CostOfRevenueSortRule, 
+    ExpenseSortRule, 
+    IgnoreSortRule, 
+    OtherExpenseSortRule, 
+    OtherIncomeSortRule,
+    RevenueSortRule,
+)
 from dao_treasury.treasury import Treasury
 
 
-__all__ = ["Treasury", "TreasuryWallet"]
+__all__ = [
+    "Treasury", 
+    "TreasuryWallet",
+    "CostOfRevenueSortRule", 
+    "ExpenseSortRule", 
+    "IgnoreSortRule", 
+    "OtherExpenseSortRule", 
+    "OtherIncomeSortRule", 
+    "RevenueSortRule",
+]
--- dao_treasury/sorting/__init__.py
+++ dao_treasury/sorting/__init__.py
@@ -2,7 +2,25 @@
 This module contains logic for sorting transactions into various categories.
 """
 from dao_treasury.sorting._matchers import _Matcher, FromAddressMatcher, HashMatcher, ToAddressMatcher
-from dao_treasury.sorting.rule import SortRule
+from dao_treasury.sorting.rule import (
+    CostOfRevenueSortRule, 
+    ExpenseSortRule, 
+    IgnoreSortRule, 
+    OtherExpenseSortRule, 
+    OtherIncomeSortRule, 
+    RevenueSortRule,
+)
 
 
-__all__ = ["SortRule", "HashMatcher", "FromAddressMatcher", "ToAddressMatcher", "_Matcher"]
+__all__ = [
+    "CostOfRevenueSortRule", 
+    "ExpenseSortRule", 
+    "IgnoreSortRule", 
+    "OtherExpenseSortRule", 
+    "OtherIncomeSortRule", 
+    "RevenueSortRule",
+    "HashMatcher", 
+    "FromAddressMatcher", 
+    "ToAddressMatcher", 
+    "_Matcher",
+]
