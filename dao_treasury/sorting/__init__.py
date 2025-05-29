500b9f757385e3d2258ad82bd34423fbe7f191eb
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-29 19:57:15 -0400

    chore: rename decorator -> factory (#62)

--- dao_treasury/sorting/__init__.py
+++ dao_treasury/sorting/__init__.py
@@ -10,7 +10,7 @@ from evmspec.data import TransactionHash
 from dao_treasury import db
 from dao_treasury._wallet import TreasuryWallet
 from dao_treasury.sorting._matchers import _Matcher, FromAddressMatcher, HashMatcher, ToAddressMatcher
-from dao_treasury.sorting.decorator import (
+from dao_treasury.sorting.factory import (
     cost_of_revenue,
     expense,
     ignore,
