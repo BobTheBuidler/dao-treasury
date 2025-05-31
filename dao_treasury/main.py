8cc6ec8e88ceffbf13b10b79a48546fa84a82e82
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-30 20:03:59 -0400

    chore: auto-update documentation dao_treasury/main.py (#74)

--- dao_treasury/main.py
+++ dao_treasury/main.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser(
-    description="Runs the DAO Treasury Exporter until stopped",
+    description="Run a single DAO Treasury export and populate the database.",
 )
 
 parser.add_argument(
@@ -25,7 +25,10 @@ parser.add_argument(
 parser.add_argument(
     "--sort-rules",
     type=Path,
-    help="The directory where your sort rules are defined. If not provided, transactions will be exported without sorting.",
+    help=(
+        "Directory containing sort rules definitions. "
+        "If omitted, transactions are exported without custom sorting."
+    ),
     default=None,
 )
 # TODO pass interval to the eth-portfolio portfolio exporter, but make the dashboard files more specific to dao treasury-ing
@@ -43,13 +46,13 @@ parser.add_argument(
 parser.add_argument(
     '--grafana-port',
     type=int,
-    help='The port that will be used by grafana',
+    help="Port for the DAO Treasury dashboard web interface. Default: 3000",
     default=3000,
 )
 parser.add_argument(
     '--renderer-port',
     type=int,
-    help='The port that will be used by grafana',
+    help="Port for the Grafana rendering service. Default: 8091",
     default=8091,
 )
 
@@ -58,11 +61,38 @@ args = parser.parse_args()
 os.environ['GF_PORT'] = str(args.grafana_port)
 os.environ['RENDERER_PORT'] = str(args.renderer_port)
 
+
 # TODO: run forever arg
 def main() -> None:
+    """Run the export process synchronously by invoking the asynchronous export.
+
+    This function is the entrypoint for the `dao-treasury` console script. It
+    parses command-line arguments and invokes :func:`export`.
+
+    Examples:
+        From the command line::
+
+            $ dao-treasury --network mainnet --sort-rules=./rules --wallet 0xABC123...
+    """
     asyncio.run(export(args))
 
+
 async def export(args) -> None:
+    """Perform a one-time export of treasury transactions and start Grafana services.
+
+    This coroutine instantiates a :class:`dao_treasury.Treasury` object using
+    `args.wallet` and `args.sort_rules`, then ensures that the necessary Docker
+    containers (Grafana and the renderer) are running before populating the
+    database with transactions from block 0 to the current chain height.
+
+    Args:
+        args: Parsed command-line arguments with attributes:
+            - wallet: Wallet address(es) for the DAO treasury.
+            - sort_rules: Directory of sorting rules.
+            - daemon: Whether to run as a daemon (currently ignored).
+            - grafana_port: Port for Grafana.
+            - renderer_port: Port for the renderer service.
+    """
     from dao_treasury import _docker, Treasury
     # TODO pass interval to the eth-portfolio portfolio exporter, but make the dashboard files more specific to dao treasury-ing
     #interval = parse_timedelta(args.interval)
