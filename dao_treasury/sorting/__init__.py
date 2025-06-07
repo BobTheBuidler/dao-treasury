77eacb8572785ac93236656ebddb5d9d42ea8059
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-06-07 22:07:55 +0000

    feat: set transaction groups asynchronously

--- dao_treasury/sorting/__init__.py
+++ dao_treasury/sorting/__init__.py
@@ -161,6 +161,7 @@ async def sort_advanced(entry: db.TreasuryTx) -> TxGroupDbid:
         must_sort_outbound_txgroup_dbid,
     ):
         logger.info("Sorted %s to %s", entry, TxGroup.get_fullname(txgroup_dbid))
+        await entry._set_txgroup(txgroup_dbid)
         entry.txgroup = txgroup_dbid
 
     return txgroup_dbid  # type: ignore [no-any-return]
