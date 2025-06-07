0d51142e02accf8a5e07659f8fa4fa6e5fd55508
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-06-07 13:21:23 -0400

    Update constants.py

--- dao_treasury/constants.py
+++ dao_treasury/constants.py
@@ -3,8 +3,6 @@ from typing import Final
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
-DISPERSE_APP: Final = (
-    "0xD152f549545093347A162Dce210e7293f1452150",
-    "0xd15fE25eD0Dba12fE05e7029C88b10C25e8880E3",
-)
+# TODO: move disperse.app stuff from yearn-treasury to dao-treasury and then write a docs file
+DISPERSE_APP: Final = ("0xD152f549545093347A162Dce210e7293f1452150", "0xd15fE25eD0Dba12fE05e7029C88b10C25e8880E3")
 """If your treasury sends funds to disperse.app, we create additional txs in the db so each individual send can be accounted for."""
