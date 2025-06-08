8f6363a4739022f078ba3307457467f21bb4b2a4
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-06-08 21:34:10 +0000

    feat: DAO_TREASURY_SQL_DEBUG env var

--- dao_treasury/__init__.py
+++ dao_treasury/__init__.py
@@ -1,3 +1,4 @@
+from dao_treasury import ENVIRONMENT_VARIABLES as ENVS
 from dao_treasury._nicknames import setup_address_nicknames_in_db
 from dao_treasury._wallet import TreasuryWallet
 from dao_treasury.db import TreasuryTx
@@ -22,6 +23,10 @@ from dao_treasury.treasury import Treasury
 setup_address_nicknames_in_db()
 
 
+if ENVS.SQL_DEBUG:
+    import pony.orm
+    pony.orm.sql_debug(True)
+    
 __all__ = [
     "Treasury",
     "TreasuryWallet",
