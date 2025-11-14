d1e2c04ee8e1382954dceaea27f53df6174a12b3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-11-14 02:14:00 +0000

    fix: port should be int

--- dao_treasury/db.py
+++ dao_treasury/db.py
@@ -84,8 +84,8 @@ EventItem = _EventItem[_EventItem[OrderedDict[str, Any]]]
 POSTGRES_USER = os.getenv("DAO_TREASURY_DB_USER", "dao_treasury")
 POSTGRES_PASSWORD = os.getenv("DAO_TREASURY_DB_PASSWORD", "dao_treasury")
 POSTGRES_DB = os.getenv("DAO_TREASURY_DB_NAME", "dao_treasury")
-POSTGRES_HOST = os.getenv("DAO_TREASURY_DB_HOST", "http://127.0.0.1")
-POSTGRES_PORT = os.getenv("DAO_TREASURY_DB_PORT", "8675")
+POSTGRES_HOST = os.getenv("DAO_TREASURY_DB_HOST", "127.0.0.1")
+POSTGRES_PORT = int(os.getenv("DAO_TREASURY_DB_PORT", "8675"))
 
 _INSERT_THREAD = AsyncThreadPoolExecutor(1)
 _SORT_THREAD = AsyncThreadPoolExecutor(1)
