609e70e24ab08def68de90d092caf2710bdfbeb7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-14 13:44:00 -0400

    Update test_wallet.py

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -294,3 +294,4 @@ def test_end_block_property_errors(end_block, end_timestamp, expected_exception,
             end_block=end_block,
             end_timestamp=end_timestamp,
         )._end_block
+
